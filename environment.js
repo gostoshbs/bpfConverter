e90069c83c96ac8eb4b9e69841bc287799c2ca57
gostoshbs <gostosh@brightsign.biz>
2017-12-11 15:48:52 -0800

    remove unused env variables

--- environment.js
+++ environment.js
@@ -1,7 +1,5 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  mode: process.env.DEPLOYMENT_MODE || 'hmr',
-  port: process.env.PORT || 3000,
   platform: process.env.PLATFORM || 'desktop'
 };
 
@@ -9,19 +7,9 @@ const config = {
 // ENVIRONMENT / DEPENDENCY VARIABLES
 // ------------------------------------
 
-let TMP = JSON.stringify('');//webtop no-op
-if(config.platform === 'desktop' && config.env === 'development'){//dev desktop variable
-  TMP = JSON.stringify('./temp');//TODO windows dev case
-} else if(config.platform === 'desktop' && config.env === 'production'){//production desktop variable
-  TMP = `require('electron').remote.app.getAppPath().replace('app.asar', 'app.asar.unpacked')`;
-}
+const BS_DEVICE_ARTIFACT_PATH = JSON.stringify('./build/static');//TODO windows dev case
+const TMP = JSON.stringify('./temp');//TODO windows dev case
 
-let BS_DEVICE_ARTIFACT_PATH = JSON.stringify('./static');//webtop case
-if(config.platform === 'desktop' && config.env === 'development'){//dev desktop variable
-  BS_DEVICE_ARTIFACT_PATH = JSON.stringify('./dist/static');//TODO windows dev case
-} else if(config.platform === 'desktop' && config.env === 'production'){//production desktop variable
-  BS_DEVICE_ARTIFACT_PATH = `require('electron').remote.app.getAppPath() + (navigator.userAgent.indexOf('Windows') > -1 ? '\\\\static' : '/static')`;
-}
 config.globals = {
   'process.env': {
     'BS_DEVICE_ARTIFACT_PATH': BS_DEVICE_ARTIFACT_PATH,
